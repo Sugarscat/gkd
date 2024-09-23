6bdf9405ed7cec421884ceab2dcad9e292e54549
Sugarscat <work@sugarscat.com>
2024-09-23 09:19:33 +0800

    Update SnapshotActionService.kt

--- app/src/main/kotlin/com/sugarscat/jump/debug/SnapshotActionService.kt
+++ app/src/main/kotlin/com/sugarscat/jump/debug/SnapshotActionService.kt
@@ -3,10 +3,10 @@ package com.sugarscat.jump.debug
 import android.app.Service
 import android.content.Intent
 import android.os.Binder
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import com.sugarscat.jump.appScope
 import com.sugarscat.jump.util.launchTry
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 /**
  * https://github.com/gkd-kit/gkd/issues/253
@@ -15,11 +15,11 @@ class SnapshotActionService : Service() {
     override fun onBind(intent: Intent?): Binder? = null
     override fun onCreate() {
         super.onCreate()
-        com.sugarscat.jump.appScope.launch {
+        appScope.launch {
             delay(1000)
             stopSelf()
         }
-        com.sugarscat.jump.appScope.launchTry {
+        appScope.launchTry {
             SnapshotExt.captureSnapshot()
         }
     }
